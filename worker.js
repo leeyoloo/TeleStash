e194674a1bc1c1c7503b90a1f35b5b6e1707511f
leeyoloo <leeyoloo@users.noreply.github.com>
2026-05-03 14:10:16 +0800

    fix: add missing formatSize function for OPDS handler

--- worker.js
+++ worker.js
@@ -613,6 +613,13 @@ function opdsFeed(title, entries, baseUrl) {
   return xml;
 }
 
+function formatSize(b) {
+  if (b < 1024) return b + " B";
+  if (b < 1048576) return (b / 1024).toFixed(1) + " KB";
+  if (b < 1073741824) return (b / 1048576).toFixed(1) + " MB";
+  return (b / 1073741824).toFixed(2) + " GB";
+}
+
 function escXml(s) {
   return String(s).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 }
